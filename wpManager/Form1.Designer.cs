97f90cce44fc4767a3be51595f6278e4b6dd1085
akhil1102 <akhilb1102@gmail.com>
2018-02-09 10:53:11 -0800

    Add project files.

---
+++ wpManager/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace wpManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_apply = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.AddFolder = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_apply
+            // 
+            this.button_apply.Location = new System.Drawing.Point(56, 47);
+            this.button_apply.Name = "button_apply";
+            this.button_apply.Size = new System.Drawing.Size(75, 23);
+            this.button_apply.TabIndex = 0;
+            this.button_apply.Text = "Browse";
+            this.button_apply.UseVisualStyleBackColor = true;
+            this.button_apply.Click += new System.EventHandler(this.button_browse_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(854, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Apply";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_apply_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1 hour",
+            "2 hours",
+            "3 hours"});
+            this.comboBox1.Location = new System.Drawing.Point(662, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(621, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hours";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(164, 142);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(396, 184);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // AddFolder
+            // 
+            this.AddFolder.Location = new System.Drawing.Point(164, 350);
+            this.AddFolder.Name = "AddFolder";
+            this.AddFolder.Size = new System.Drawing.Size(75, 23);
+            this.AddFolder.TabIndex = 6;
+            this.AddFolder.Text = "Add Folder";
+            this.AddFolder.UseVisualStyleBackColor = true;
+            this.AddFolder.Click += new System.EventHandler(this.AddFolder_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(283, 350);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 543);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.AddFolder);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_apply);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_apply;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button AddFolder;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ wpManager/Form1.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace wpManager
+{
+    public partial class Form1 : Form
+    {
+        public string[] allFiles = new string[10];
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, string pvParam, uint fWinIni);
+        const uint SPI_SETDESKWALLPAPER = 0x14;
+        const uint SPIF_UPDATEINIFILE = 0x01;
+        const uint SPIF_SENDWININICHANGE = 0x02;
+        public Form1()
+        {
+            InitializeComponent();
+            SystemEvents.PowerModeChanged += OnPowerChange;
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(EventLock);
+
+        }
+        private void EventLock(object sender, SessionSwitchEventArgs e)
+        {
+            if(e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                string path = @"F:\others\wallpapers\";
+                string[] files = Directory.GetFiles(path,"*jpg");
+                var rand = new Random();
+                string randomFile = files[rand.Next(files.Length)];
+                var res = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, randomFile, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+                //Console.WriteLine(GetWPPath());
+            }
+        }
+        private void OnPowerChange(object sender, PowerModeChangedEventArgs e)
+        {
+            switch (e.Mode)
+            {
+                case PowerModes.Resume:
+                    System.Timers.Timer myTimer = new System.Timers.Timer();
+                    string selectedTime = comboBox1.SelectedItem.ToString();
+                    myTimer.Interval = (Int32.Parse(selectedTime.Substring(0, 1))) * 60 * 60 * 1000;  //time in milliseconds
+                    myTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent2);
+                    myTimer.AutoReset = false;
+                    myTimer.Enabled = true;
+                    //Console.WriteLine(result);
+                    //string wp = GetWPPath();
+                    //Console.WriteLine(wp);
+                    break;
+                //case PowerModes.Suspend:
+                //    var res = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, @"F:\others\b9fe47a98226cffc682ffaafb0014a90f703eae5", SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+                //    break;
+            }
+
+        }
+        private void OnTimedEvent2(object source, ElapsedEventArgs e)
+        {
+            string file = @"F:\others\batman_v_superman-1366x768";
+            var result = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, file, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            Console.WriteLine("wallpaper set:" + result);
+            Console.WriteLine(GetWPPath());
+        }
+        private static string GetWPPath()
+        {
+            RegistryKey wallpaper = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", false);
+            string wp_path = wallpaper.GetValue("WallPaper").ToString();
+            wallpaper.Close();
+            return wp_path;
+        }
+
+        private void button_browse_Click(object sender, EventArgs e)        //clicking button browse opens the dialog box to select image
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".jpeg";
+            dlg.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            dlg.Multiselect = true;
+            
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                PictureBox[] picBoxes = new PictureBox[dlg.FileNames.Length];
+                int i = 0;
+                int x = 0;
+                for (int j = 0; j < dlg.FileNames.Length; j++)
+                {
+                    allFiles[j] = dlg.FileNames[j];
+                }
+                foreach (string file in dlg.FileNames)
+                {
+                    picBoxes[i] = new PictureBox
+                    {
+                        Location = new System.Drawing.Point(30 + x, 106),
+                        Size = new System.Drawing.Size(100, 117),
+                        Name = "picturebox" + i,
+                        Image = Image.FromFile(file),
+                        SizeMode = PictureBoxSizeMode.StretchImage
+                    };
+                    this.Controls.Add(picBoxes[i]);
+                    i++;
+                    x += 130;
+                }
+
+            }
+        }
+        
+        private void button_apply_Click(object sender, EventArgs e)
+        {
+            System.Timers.Timer myTimer = new System.Timers.Timer();
+            string s = comboBox1.SelectedItem.ToString();
+            myTimer.Interval = (Int32.Parse(s.Substring(0, 1))) * 60 * 60 * 1000;  //time in milliseconds
+            myTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            myTimer.Enabled = true;
+            
+        }
+
+        private void OnTimedEvent(object source,ElapsedEventArgs e)
+        {
+            SetWallpaper(allFiles);
+        }
+
+        static int i = 0;
+        public void SetWallpaper(string[] files)
+        {
+            if (i <= files.Length)
+            {
+                string file = files[i];
+                var result = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, file, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+                i++;
+                if (i == files.Length)
+                {
+                    i = 0;
+                }
+            }
+        }
+
+        private void AddFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            DialogResult res = fd.ShowDialog();
+            if(res == DialogResult.OK && !string.IsNullOrWhiteSpace(fd.SelectedPath))
+            {
+                string path = fd.SelectedPath;
+                var items = checkedListBox1.Items;
+                items.Add(path, true);
+            }
+
+            string dir = Environment.CurrentDirectory;
+            string wordfile = dir + "\\data.txt";
+            if (File.Exists(wordfile))
+            {
+                var sw = new StreamWriter(wordfile, true);
+                foreach(string path in checkedListBox1.Items)
+                {
+                    sw.WriteLine(path);
+                }
+            }
+
+
+        }
+    }
+}
+
+
+       
